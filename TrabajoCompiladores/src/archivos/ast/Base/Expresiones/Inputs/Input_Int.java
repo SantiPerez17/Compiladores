5580098fb8dfd3c9b15045a7289b396b7a921230
SantiPerez17 <55918957+SantiPerez17@users.noreply.github.com>
2022-06-06 13:17:59 -0300

    input_int arreglao

--- TrabajoCompiladores/src/archivos/ast/Base/Expresiones/Inputs/Input_Int.java
+++ TrabajoCompiladores/src/archivos/ast/Base/Expresiones/Inputs/Input_Int.java
@@ -24,12 +24,14 @@ public class Input_Int extends Expresion {
     public String generarCodigo(String etiqueta) {
         StringBuilder resultado = new StringBuilder();
         this.setIr_ref(CodeGeneratorHelper.getNewPointer());
-        String dest = this.getIr_ref();
+        String destaux = this.getIr_ref();
         this.setIr_ref(CodeGeneratorHelper.getNewPointer());
         String temp = this.getIr_ref();
-        resultado.append(dest + " = alloca i32\n");
-        resultado.append(String.format("%1$s = call i32 (i8*, ...) @scanf(i8* getelementptr inbounds ([3 x i8], [3 x i8] * @int_read_format, i64 0, i64 0), i32* %2$s)\n", temp, dest));
-        resultado.append(String.format("%1$s = load i32, i32* %2$s\n", temp, dest));
+        this.setIr_ref(CodeGeneratorHelper.getNewPointer());
+        String temp_int = this.getIr_ref();
+        resultado.append(destaux + " = alloca i32\n");
+        resultado.append(String.format("%1$s = call i32 (i8*, ...) @scanf(i8* getelementptr inbounds ([3 x i8], [3 x i8] * @int_read_format, i64 0, i64 0), i32* %2$s)\n", temp, destaux));
+        resultado.append(String.format("%1$s = load i32, i32* %2$s\n", temp_int, destaux));
         return resultado.toString();
     }
 }
